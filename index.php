a908d0ebb36c4755d0f5425d8d4aef3f6cf63e30
TristanDiabolo <tristan@diabolo-web.com>
2020-01-30 11:50:30 +0100

    organize index.php

--- index.php
+++ index.php
@@ -13,25 +13,21 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 
 // GLOBAL VARIABLES
 $twig->addGlobal('isLogged', UserManager::checkIsLogged());
-$twig->addGlobal('base_url', Config::BASE_URL);
-// add global user info
 $twig->addGlobal('username', UserManager::getUsername());
 $twig->addGlobal('userEmail', UserManager::getEmail());
+$twig->addGlobal('base_url', Config::BASE_URL);
 
+// NAVIGATION
 $pages = get_class_methods('PageController');
-$urls = array();
 foreach ($pages as $page) {
     $pageUrl = Config::BASE_URL . "/?page=" . $page;
     $twig->addGlobal($page . "_url", $pageUrl);
 }
 
-// NAVIGATION
 $page = Globals::get('get', 'page');
-
 if (!isset($page) || null == $page) {
     $page = 'home';
 }
-
 $twig->addGlobal('page_name', $page);
 
 // LOGIN FORM

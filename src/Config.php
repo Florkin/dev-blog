d997244563cf755694d8a31b7af897e80c4a59ae
TristanDiabolo <tristan@diabolo-web.com>
2020-02-12 20:29:55 +0100

    change local database name

--- src/Config.php
+++ src/Config.php
@@ -2,7 +2,7 @@
 
 abstract class Config {
 
-    const DB_NAME = "dev_blog";
+    const DB_NAME = "dev-blog";
     const DB_USER = "root";
     const DB_PASSWORD = "";
     const DB_HOST = "localhost";

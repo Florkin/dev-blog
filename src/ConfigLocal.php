985daa48d4c40db94afc6d9a04b748e6f278cdb8
TristanDiabolo <tristan@diabolo-web.com>
2020-06-18 18:12:49 +0200

    Add empty configLocal.php

---
+++ src/ConfigLocal.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+/**
+ * Set global constants
+ */
+abstract class Config {
+
+    const DB_NAME = "dev-blog"; // Database name
+    const DB_USER = "root"; // Database user
+    const DB_PASSWORD = ""; // Database password
+    const DB_HOST = "localhost"; // Database Host
+    const DEBUG = true; // Enable Debug mode and disable Twig cache
+    const EMAIL_HOST = ""; // ex: "smtp.gmail.com"
+    const EMAIL_FROM = ""; // // Address from which the emails will be sent
+    const EMAIL_PASSWORD = ""; // To connect to gmail account, for example
+    const EMAIL_USERNAME = ""; // To connect to gmail account, for example
+    const EMAIL_CONTACT = ""; // Contact form will send mails at this address
+
+}

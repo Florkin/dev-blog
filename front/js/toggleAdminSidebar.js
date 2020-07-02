ca90a3f08d72b63c5d1ed32582e4fb60ea2c0c8a
TristanDiabolo <tristan@diabolo-web.com>
2020-07-02 11:24:12 +0200

    Templating Dashboard

--- front/js/toggleAdminSidebar.js
+++ front/js/toggleAdminSidebar.js
@@ -1,11 +1,4 @@
-$("#menu-toggle").click(function(e) {
+$(".menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
-
-    if ($("#wrapper").hasClass("toggled")){
-        $("#menu-toggle").text('fermer')
-    } else {
-        $("#menu-toggle").text('Menu')
-
-    }
 });
